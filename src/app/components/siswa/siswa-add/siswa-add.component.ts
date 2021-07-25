06c27b776869d45fc9b497100d26d743c48d3827
andijayawizard <33024329+andijayawizard@users.noreply.github.com>
2021-07-25 18:08:58 +0700

    2507

--- src/app/components/siswa/siswa-add/siswa-add.component.ts
+++ src/app/components/siswa/siswa-add/siswa-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Siswa } from 'src/app/models/siswa.model';
+import { SiswaService } from 'src/app/services/siswa.service';
 
 @Component({
   selector: 'app-siswa-add',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./siswa-add.component.css']
 })
 export class SiswaAddComponent implements OnInit {
+  siswa: Siswa = {
+    nama: '',
+    alamat: '',
+    isActive: false
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private siswaService: SiswaService) { }
 
   ngOnInit(): void {
+  }
+  saveSiswa(): void {
+    const data = {
+      nama: this.siswa.nama,
+      alamat: this.siswa.alamat,
+    };
+
+    this.siswaService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newSiswa(): void {
+    this.submitted = false;
+    this.siswa = {
+      nama: '',
+      alamat: '',
+      isActive: false
+    };
   }
 
 }
--- src/app/components/siswa/siswa-detail/siswa-detail.component.ts
+++ src/app/components/siswa/siswa-detail/siswa-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SiswaService } from 'src/app/services/siswa.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Siswa } from 'src/app/models/siswa.model';
 
 @Component({
   selector: 'app-siswa-detail',
@@ -6,10 +9,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./siswa-detail.component.css']
 })
 export class SiswaDetailComponent implements OnInit {
+  currentSiswa: Siswa = {
+    nama: '',
+    alamat: '',
+    isActive: false
+  };
+  message = '';
 
-  constructor() { }
+  constructor(
+    private siswaService: SiswaService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.params.id);
+  }
+  getTutorial(id: string): void {
+    this.siswaService.get(id)
+      .subscribe(
+        data => {
+          this.currentSiswa = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePublished(status: boolean): void {
+    const data = {
+      nama: this.currentSiswa.nama,
+      alamat: this.currentSiswa.alamat,
+      isActive: status
+    };
+
+    this.message = '';
+
+    this.siswaService.update(this.currentSiswa.id, data)
+      .subscribe(
+        response => {
+          this.currentSiswa.isActive = status;
+          console.log(response);
+          this.message = response.message ? response.message : 'This tutorial was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateTutorial(): void {
+    this.message = '';
+
+    this.siswaService.update(this.currentSiswa.id, this.currentSiswa)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = response.message ? response.message : 'This tutorial was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  deleteTutorial(): void {
+    this.siswaService.delete(this.currentSiswa.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/tutorials']);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
